0edeea8593969138600c8e0b4ce1d0c45a18361d
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 17:00:50 +0100

    fix include files

--- xmake.lua
+++ xmake.lua
@@ -10,6 +10,7 @@ set_warnings("all", "error")
 target("lys-types")
     set_kind("headeronly")
     add_includedirs("include", {public = true})
+    add_headerfiles("include/**.hpp", {public = true})
 
 for _, testfile in ipairs(os.files("tests/**.cpp")) do 
     local name = path.basename(testfile)
