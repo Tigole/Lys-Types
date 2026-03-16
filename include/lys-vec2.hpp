bddfb2f52bc2d08021128df05d40c397f5b300a7
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 22:34:36 +0100

    add more vec2

--- include/lys-vec2.hpp
+++ include/lys-vec2.hpp
@@ -13,8 +13,14 @@ struct Vec2
     T y;
 };
 
+using Vec2u64 = Vec2<uint64_t>;
 using Vec2u32 = Vec2<uint32_t>;
 using Vec2u16 = Vec2<uint16_t>;
+using Vec2u8  = Vec2<uint8_t>;
+using Vec2i64 = Vec2<int64_t>;
+using Vec2i32 = Vec2<int32_t>;
+using Vec2i16 = Vec2<int16_t>;
+using Vec2i8  = Vec2<int8_t>;
 using Vec2f32 = Vec2<float>;
 using Vec2f64 = Vec2<double>;
 
